4f5867b61c10c98af0fb8f722b9cf4f33f2e847c
Holger Janßen-Kroll <post@holgerjanssen.de>
2017-12-13 20:18:42 +0100

    first debugging

--- src/classes/ImageHelper.php
+++ src/classes/ImageHelper.php
@@ -4,24 +4,24 @@ namespace MUK\Images;
 
 class ImageHelper extends \Controller {
 	
-	private const $configuration = array (
+	private static $configuration = array (
 		"left" => array (
 			"join_image"    => 'system/modules/muk_images/assets/img/arrow_right.png',
 			'join_position' => array (100, 100),
-			'circle_color'  => '',
-			'target_size'   => array (),
-			'source_size'   => array (),
+			'circle_color'  => 'green',
+			'target_size'   => array (600,600),
+			'source_size'   => array (400, 400),
 			'cut_left'      =>  0,
 			'cut_right'     =>  0,
 			'cut_top'       =>  0,
 			'cut_bottom'    =>  0,
 			'background'    => 'red',
 			'file_suffix'   => 'nl',
-		);
+		),
 	);
 
 
-	private static fucntion getContaoImage ( $image, $width, $height ) {
+	private static function getContaoImage ( $image, $width, $height ) {
 		if ( \Validator::isUuid( $image ) ) {
 			$imgFile = \FilesModel::findByPk ( $image );
 		  	$image = $imgFile->path;
@@ -48,13 +48,13 @@ class ImageHelper extends \Controller {
 
 
 		// create empty image
-		$createImage = new Imagick();
-		$createImage->newImage ( $config['target_size'][0], $config['target_size'][1], new ImagickPixel( $config['background'] );
+		$createImage = new \Imagick();
+		$createImage->newImage ( $config['target_size'][0], $config['target_size'][1], new \ImagickPixel( $config['background'] ));
 		$createImage->setImageFormat ('png');
 
 
 		// draw frame circle
-		$drawCircle = new ImagickDraw ();
+		$drawCircle = new \ImagickDraw ();
 		$drawCircle->setStrokeAntialias ( true );
 		$drawCircle->setFillColor ( $config['circle_color'] );
 		$drawCircle->circle(0,0,$config['target_size'][0], $config['target_size'][1] );
@@ -63,28 +63,28 @@ class ImageHelper extends \Controller {
 
 		
 		// add cut part from the existing image
-		$mask = new Imagick();
-		$mask->newImage ( $config['source_size'][0], $config['source_size'][1], new ImagickPixel ('white') );
-		$drawMaskCircle = new ImagickDraw ();
+		$mask = new \Imagick();
+		$mask->newImage ( $config['source_size'][0], $config['source_size'][1], new \ImagickPixel ('white') );
+		$drawMaskCircle = new \ImagickDraw ();
 		$drawMaskCircle ->setStrokeAntialias ( true );
-		$drawMaskCircle ->setFillColor ( new ImagickPixel ('black'));
+		$drawMaskCircle ->setFillColor ( new \ImagickPixel ('black'));
 		$drawMaskCircle ->circle ( 0, 0, $config['source_size'][0], $config['source_size'][1] );
 		$mask->drawImage ( $drawMaskCircle );
 		
-		$original = new Imagick ( $contaoImage );
-		$original->compositeImage ( $mask, Imagick::COMPOSITE_COPYOPACITY, 0, 0 );
+		$original = new \Imagick ( $contaoImage );
+		$original->compositeImage ( $mask, \Imagick::COMPOSITE_COPYOPACITY, 0, 0 );
 
 		$createImage->compositeImage ( 
-			$orignal, 
-			Imagick::COMPOSITE_COPYOPACITY, 
+			$original, 
+			\Imagick::COMPOSITE_COPYOPACITY, 
 			(config['target_size'][0] - $config['source_size'][0]) / 2,
 			(config['target_size'][1] - $config['source_size'][1]) / 2
 		);
 
 
 		// add the arrow image
-		$joinImage = new Imagick ( $config['join_image'] );
-		$createImage->compositeImage ( $joinImage, imagick::COMPOSITE_COPYOPACITY, $config['join_position'][0], $config['join_position'][1]);
+		$joinImage = new \Imagick ( $config['join_image'] );
+		$createImage->compositeImage ( $joinImage,\Imagick::COMPOSITE_COPYOPACITY, $config['join_position'][0], $config['join_position'][1]);
 
 
 		// store the image in the image cache
